606ff7ef7b5e47dfd4d5257602eb9e33814a0e8f
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-14 19:22:31 +0100

    Values can be seen, but implementation need to be cleaned up

--- rgb_reader/src/main.rs
+++ rgb_reader/src/main.rs
@@ -1,30 +1,47 @@
 extern crate image;
 
 use crate::image::GenericImageView;
+use fltk::*;
 use std::path::Path;
 
-use fltk::{app::*, button::*,menu::*, dialog::*, window::*};
+use fltk::{app::*, button::*, dialog::*, output::*, window::*};
 
 fn main() {
     let app = App::default();
     let mut wind = Window::new(100, 100, 400, 300, "R G B Image Processor.");
-    let mut but = Button::new(160, 210, 80, 40, "Process it!");
-    wind.end();
+    let mut but = Button::new(100, 100, 80, 40, "Process it!");
+    let mut out_r = Output::new(100, 200, 30, 30, "");
+    let mut out_g = Output::new(150, 200, 30, 30, "");
+    let mut out_b = Output::new(200, 200, 30, 30, "");
+
+    out_r.set_text_size(14);
+    out_g.set_text_size(14);
+    out_b.set_text_size(14);
+
     wind.show();
-    but.set_callback(|| {
+    but.set_callback(move|| {
         if let Some(file) = file_chooser("Choose a file", "*.jpg", ".", true) {
             let con = &file[..];
-            let popup_options = vec!["OK"];
-            let mut menu_item = MenuItem::new(&popup_options);
             if Path::new(con).exists() {
                 let rgbs = read_pix_from_file(con);
-            }
-            match menu_item.popup(100,100) {
-                None => println!("None"),
-                Some(val) => println!("{}", val.label().unwrap()),
+                println!("{} {} {}", rgbs.0, rgbs.1, rgbs.2);
+
+
+                let out_r_string = rgbs.0.to_string();
+                let out_g_string = rgbs.1.to_string();
+                let out_b_string = rgbs.2.to_string();
+                let out_r_sliced=&out_r_string;
+                let out_g_sliced=&out_g_string;
+                let out_b_sliced=&out_b_string;
+                out_r.set_value(out_r_sliced);
+                out_g.set_value(out_g_sliced);
+                out_b.set_value(out_b_sliced);
             }
         }
     });
+    wind.redraw();
+    wind.end();
+
     app.run().unwrap();
 }
 
